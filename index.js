51a26be15f693ddfd66c9fcd8477f227c2b3fd26
PatrickJS <github@gdi2290.com>
2016-08-06 19:04:49 -0700

    chore: src

---
+++ index.js
@@ -0,0 +1,28 @@
+var ConcatSource = require('webpack-core/lib/ConcatSource');
+var corejsBuilder = require('core-js-builder');
+
+function CoreJsPlugin(options) {
+  this.options = options || {};
+}
+
+CoreJsPlugin.prototype.apply = function CoreJsPluginApply (compiler) {
+  var self = this;
+  compiler.plugin('compilation', function(compilation) {
+    compilation.plugin('optimize-chunk-assets', function(chunks, callback) {
+      chunks.forEach(function(chunk) {
+        if (!chunk.initial) return;
+        corejsBuilder(self.options)
+          .then(function(bundledCode) {
+            chunk.files.forEach(function(file) {
+              var src = new ConcatSource('/* core-js */\n', bundledCode, compilation.assets[file]);
+              compilation.assets[file] = src
+            });
+            callback();
+
+          });// end then
+      });// end for each
+    });// end optimize-chunk-assets
+  });// end compilation
+};// end apply
+
+module.exports = CoreJsPlugin;
